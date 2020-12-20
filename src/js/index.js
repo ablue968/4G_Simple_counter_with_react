325c71b61efb79aa23863f973d76a12ca480e9c4
ablue968 <ablue968@gmail.com>
2020-12-20 21:38:16 +0000

    funciona pero quiero los botones

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
@@ -8,8 +9,40 @@ import "bootstrap";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import { Home } from "./component/home.js";
+function SimpleCounter(props) {
+	return (
+		<div className="wholeCounter">
+			<div className="calendar">
+				<i className="far fa-clock" />
+			</div>
+			<div className="fourth_digit">{props.fourth_digit % 10}</div>
+			<div className="third_digit">{props.third_digit % 10}</div>
+			<div className="second_digit">{props.second_digit % 10}</div>
+			<div className="first_digit">{props.first_digit % 10}</div>
+		</div>
+	);
+}
+SimpleCounter.PropTypes = {
+	fourth_digit: PropTypes.number,
+	third_digit: PropTypes.number,
+	second_digit: PropTypes.number,
+	first_digit: PropTypes.number
+};
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(() => {
+	const fourth_digit = Math.floor(counter / 1000);
+	const third_digit = Math.floor(counter / 100);
+	const second_digit = Math.floor(counter / 10);
+	const first_digit = Math.floor(counter / 1);
+	counter++;
+	ReactDOM.render(
+		<SimpleCounter
+			first_digit={first_digit}
+			second_digit={second_digit}
+			third_digit={third_digit}
+			fourth_digit={fourth_digit}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
